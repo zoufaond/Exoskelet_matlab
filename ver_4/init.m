03ff62e3b183dd4f678ee3fe6aa03dd710cb6940
zoufaond <zoufik.o@seznam.cz>
2024-01-09 11:45:57 +0100

    transformed

--- ver_4/init.m
+++ ver_4/init.m
@@ -1,8 +1,8 @@
 %% ellipsoid joint
 % scapula frames initial position (measured)
 S_00 = [-0.094965;+0.130202;-0.086922];
-S_10 = [-0.062046;+0.163320;-0.091534];
-S_20 = [-0.085749;+0.051924;-0.113461];
+S_10 = [-0.0915340000000000;	0.0133200000000000;	0.0620460000000000];
+S_20 = [-0.113461000000000;	-0.0980760000000000;	0.0857490000000000];
 % initial position optimization
 nvars = 2;
 lb = [-pi;h_min];
@@ -63,7 +63,7 @@ function d = dst(h,phi,S,x_ej,y_ej,z_ej,psi_ej,mu_ej,phi_ej,r_ej,h_ej)
     r = sqrt((r_ej^2)*(1-h^2/h_ej^2));
     T_t = T_tx(x_ej)*T_ty(y_ej)*T_tz(z_ej);
     T_r = T_ry(psi_ej)*T_rx(mu_ej)*T_ry(phi_ej);
-    T_ej = T_rx(-pi/2)*T_rz(phi)*T_tz(h)*T_ry(pi/2)*T_tz(r);
+    T_ej = T_rx(-pi/2)*T_rz(phi)*T_tz(h)*T_ry(-pi/2)*T_tz(r);
     R = T_t*T_r*T_ej*[0;0;0;1];
     d = sqrt((S(1)-R(1))^2+(S(2)-R(2))^2+(S(3)-R(3))^2);
 end
