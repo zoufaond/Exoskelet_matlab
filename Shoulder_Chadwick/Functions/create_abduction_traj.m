69c68d88b732fb7c0640664ff3898d9c2f3b6746
zoufaond <zoufik.o@seznam.cz>
2024-06-29 21:39:01 +0100

    change parameter

--- Shoulder_Chadwick/Functions/create_abduction_traj.m
+++ Shoulder_Chadwick/Functions/create_abduction_traj.m
@@ -1,5 +1,5 @@
 function traj = create_abduction_traj(initCond,n)
-scale = 0.5;
+scale = 0.8;
 Abduction = (sin(linspace(0,pi,n)-pi/2)'+1)*scale;
     for i=1:n
         Rhum = Humerus_to_Thorax_matrix([initCond(1:7);initCond(8)+Abduction(i);initCond(9)]);
