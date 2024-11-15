54bc6ebc95ad80f68c0b75ecf83221dd346a04e0
zoufaond <zoufik.o@seznam.cz>
2024-11-15 15:54:59 +0100

    delete unused structs

--- Shoulder_Chadwick/datatomot.m
+++
@@ -1,8 +0,0 @@
-addpath Functions\
-
-% python_sol_quat = load('quat_steering_eulIC.mat');
-% data2mot(python_sol_quat.timeseries_coords,'quat_steering_eulIC.mot', 'quaternion', 'struct')
-
-python_sol_eul = load('eul_steering_eulIC.mat');
-data2mot(python_sol_eul.timeseries_coords,'eul_steering_eulIC.mot', 'euler', 'struct')
-
